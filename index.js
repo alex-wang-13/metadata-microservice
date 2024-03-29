0f66524e80987b9b0588e6548abc40d40435ef0a
Alex Wang <iamalex11303@gmail.com>
2024-03-29 11:02:09 -0400

    responded with multer fields at api/fileanalyse

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var express = require('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
 var multer = require('multer')
+var upload = multer({dest: '/.uploads'})
 require('dotenv').config()
 
 var app = express()
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html')
 })
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file)
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, function () {
